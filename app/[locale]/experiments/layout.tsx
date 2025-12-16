db92a399b91b359b0418be3a3100bf1846e058aa
てんとう虫 <reservecrate@gmail.com>
2025-12-16 21:01:11 +0500

    refactor: create shared SidebarLayout component
    Created shared/components/layout/SidebarLayout.tsx
    
    Refactored DojoMenu, ProgressWithSidebar, Settings, TranslatorPage
    
    Refactored achievements, academy pages to use SidebarLayout
    
    Updated experiments layout to use SidebarLayout
    
    Reduces code duplication and improves maintainability

--- app/[locale]/experiments/layout.tsx
+++ app/[locale]/experiments/layout.tsx
@@ -1,18 +1,9 @@
-'use client';
-import Sidebar from '@/shared/components/Menu/Sidebar';
-import clsx from 'clsx';
+import SidebarLayout from '@/shared/components/layout/SidebarLayout';
 
 export default function ExperimentsLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className='min-h-[100dvh] max-w-[100dvw] flex'>
-      <Sidebar />
-      <div className={clsx('flex flex-col w-full lg:w-4/5 px-4 md:px-8 pb-20')}>
-        {children}
-      </div>
-    </div>
-  );
+  return <SidebarLayout showBanner={false}>{children}</SidebarLayout>;
 }
